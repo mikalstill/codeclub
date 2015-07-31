2e50803ee06c9fa0d8b38b71185a6940c3352304
Michael Still <mikal@stillhq.com>
2015-07-31 14:50:32 +1000

    Add a maximum number of guesses in the number guessing game.
    This is included as a demo of how commit history works in git as we introduce
    github.com.

--- 2015/number_guessing_game/guess.py
+++ 2015/number_guessing_game/guess.py
@@ -1,15 +1,18 @@
 import random
+import sys
 
 # Pick a random number
 number = random.randint(0, 10)
 
 # Now ask for guesses until the correct guess is made
 done = False
+attempts = 0
 
 while not done:
     guess = int(raw_input('What is your guess? '))
     print 'You guessed: %d' % guess
     
+    attempts += 1
     if guess < number:
         print 'Higher!'
     elif guess > number:
@@ -17,3 +20,7 @@ while not done:
     else:
         print 'Right!'
         done = True
+
+    if attempts > 5:
+        print 'Too many attempts!'
+        sys.exit(1)
